97fd6b4623f7a319b12c5d139cd78a9b30b9d114
YelavarthiLalitya <lalityayelavarthi@gmail.com>
2025-06-10 13:01:12 +0530

    updated

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,35 +1,45 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom';
-import App, { AppContext } from './App';
-import { useContext, useState } from 'react';
-
-
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { AppContext } from "./App";
 export default function Login() {
-  const {users} = useContext(AppContext);
   const [user, setUser] = useState({});
-  const {error, setError} = useState();  
+  const [error, setError] = useState();
   const Navigate = useNavigate();
-  
+  const { users } = useContext(AppContext);
   const handleSubmit = () => {
-    const found = users.find( (element) => element.email===user.email && element.pass===user.pass);
-    if (!found){
+    const found = users.find(
+      (elem) => elem.email === user.email && elem.pass === user.pass
+    );
+    if (!found) {
       setError("Access Denied");
-    }
-    else{
-      Navigate("/")
+    } else {
+      Navigate("/");
     }
   };
   return (
     <div>
-        <h2>Login Form</h2>
-        {error}
-        <p>< input type="text" placeholder='Enter email' onChange={(e)=>setUser({...user, email:e.target.value})}/></p>
-        <p><input type="password" placeholder='Enter password' onChange={(e)=>setUser({...user, pass:e.target.value})}/></p>
-        <p><button onClick={handleSubmit}>Login</button></p>
-        <hr />
-        <Link to='/register'>Create an Account!</Link> 
+      <h2>Login Form</h2>
+      {error}
+      <p>
+        <input
+          type="text"
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+        />
+      </p>
+      <p>
+        <input
+          type="password"
+          onChange={(e) => setUser({ ...user, pass: e.target.value })}
+        />
+      </p>
+      <p>
+        <button onClick={handleSubmit}>Login</button>
+      </p>
+      <hr />
+      <p>
+        <Link to="/register">Create Account</Link>
+      </p>
     </div>
-  )
+  );
 }
-
-///hi
