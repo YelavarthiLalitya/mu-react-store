577f88ec565c7177c264ba360ea773cd83fb4908
YelavarthiLalitya <lalityayelavarthi@gmail.com>
2025-06-10 16:25:44 +0530

    updated

--- src/Order.jsx
+++ src/Order.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { useContext } from 'react'
+import App, { AppContext } from './App'
+
 
 export default function Order() {
+    const {orders, email} = useContext(AppContext)
   return (
-    <div>Order</div>
+    <div>
+        <h2>My Order</h2>
+        {orders.map(order=>(<div key={order.id}>{order.name}-{order.price}-{order.qty}-{order.price * order.qty}</div>))}
+    </div>
   )
 }
