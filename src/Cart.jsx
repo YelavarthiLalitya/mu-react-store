9f2052a29f5a1c4c1a5a582354ec56ba98531dcf
YelavarthiLalitya <lalityayelavarthi@gmail.com>
2025-06-10 15:00:40 +0530

    updated

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,31 +1,33 @@
-import React from 'react'
-import { AppContext } from './App'
-import { useContext } from 'react'
-
+import React from "react";
+import { AppContext } from "./App";
+import { useContext } from "react";
 export default function Cart() {
-    const {cart} = useContext(AppContext)
-    const increment = () =>{
-        alert("Increment")
-        setCart({...cart,qty:cart.qty+1})
-    };
-    const decrement = () =>{
-        alert("Decrement")
-        setCart({...cart,qty:cart.qty-1})
-    };
+  const { cart,setCart } = useContext(AppContext);
+  const increment = () => {
+    setCart({ ...cart, qty: cart.qty + 1 });
+  };
+
+  const decrement = () => {
+    cart.qty > 0 && setCart({ ...cart, qty: cart.qty - 1 });
+  };
   return (
     <div>
-        <h2>My cart</h2>
-        <h3>{cart.name}</h3>
-        <p>{cart.desc}</p>
-        <h3>{cart.price}</h3>
-        <p>
-            <button onClick={decrement}>-</button>
-            {cart.qty}
-            <button onClick={increment}>+</button>
-        </p>
-        <hr />
-        <h2>Order Value:{cart.price * cart.qty}</h2>
-        
+      <h2>My Cart</h2>
+      <h3>{cart.name}</h3>
+      <p>{cart.desc}</p>
+      <h3>Price:{cart.price}</h3>
+      <p>
+        <button onClick={decrement}>-</button>
+        {cart.qty}
+        <button onClick={increment}>+</button>
+      </p>
+      <hr />
+      <h2>Order Value:{cart.price * cart.qty}</h2>
+      <hr />
+      <p>
+        <button>Place Order</button>
+        <button>Login to Order</button>
+      </p>
     </div>
-  )
+  );
 }
