048da6c47f57c52ad68816073d03b0a99fcfcb7f
YelavarthiLalitya <lalityayelavarthi@gmail.com>
2025-06-10 15:13:52 +0530

    udpated

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,35 @@
-import { useState, createContext} from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import './Footer.jsx';
-import './Header.jsx';
-import Header from './Header.jsx';
-import Footer from './Footer.jsx';
-import Home from './Home.jsx';
-import Login from './Login.jsx';
-import Cart from './Cart.jsx'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Register from './Register.jsx';
-export const AppContext = createContext()
+import { useState, createContext } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./Header";
+import Footer from "./Footer";
+import Login from "./Login";
+import Cart from "./Cart";
+import Register from "./Register";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+export const AppContext = createContext();
 function App() {
-  //const [user, setUser] = useState({});
-  const [users, setUsers] = useState([]);
-  const [cart, setCart] = useState({});
-  const [email, setEmail] = useState({});
-
+  const [users,setUsers] = useState([])
+  const [cart,setCart] = useState({})
+  const [email,setEmail] = useState()
   return (
     <div>
-    <AppContext.Provider value={{users, setUsers, cart, setCart, email, setEmail}}>
-      <BrowserRouter>
-        <Header name="Mu-React-Store" />
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <AppContext.Provider value={{users,setUsers,cart,setCart,email,setEmail}}>
+        <BrowserRouter>
+          <Header name="mu-react-store" />
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
       </AppContext.Provider>
     </div>
   );
 }
-
 export default App;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,58 +1,56 @@
-import React, { useState } from 'react';
-import './Home.css';
+import React from "react";
+import "./Home.css";
+import { AppContext } from "./App";
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import { AppContext } from './App';
-import { useContext } from 'react';
-
-
 export default function Home() {
-const { cart, setCart, email } = useContext(AppContext);
-    const Navigate = useNavigate();
-    const addToCart = (obj) =>{
-      setCart({id:obj.id,
-               name:obj.name,
-               price:obj.price,
-               desc:obj.desc,
-               qty:1,
-               email: email,
-              });
-      Navigate("/cart");
-    }
+  const {cart,setCart,email} = useContext(AppContext)
   const products = [
     {
       id: 1,
       name: "Product 1",
-      desc: "This is the description of the product.",
+      desc: "This is the description of the product",
       price: 45,
-      imgUrl: "https://picsum.photos/id/237/300/300"
-
+      imgUrl: "https://picsum.photos/id/1/300/300",
     },
     {
       id: 2,
       name: "Product 2",
-      desc: "This is the description of the product.",
-      price: 47,
-      imgUrl: "https://picsum.photos/id/236/300/300"
-
+      desc: "This is the description of the product",
+      price: 50,
+      imgUrl: "https://picsum.photos/id/2/300/300",
     },
     {
       id: 3,
       name: "Product 3",
-      desc: "This is the description of the product.",
-      price: 49,
-      imgUrl: "https://picsum.photos/id/235/300/300"
-    }
+      desc: "This is the description of the product",
+      price: 75,
+      imgUrl: "https://picsum.photos/id/3/300/300",
+    },
   ];
-
+  const Navigate = useNavigate();
+  const buyNow = (obj) => {
+    setCart({
+      id: obj.id,
+      name: obj.name,
+      price: obj.price,
+      desc: obj.desc,
+      qty: 1,
+      email:email,
+    });
+    Navigate("/cart");
+  };
   return (
     <div className="App-Home-Row">
-      {products.map(product => (
-        <div key={product.id}>
+      {products.map((product) => (
+        <div>
           <img src={product.imgUrl} alt={product.name} />
           <h2>{product.name}</h2>
           <p>{product.desc}</p>
-          <p>₹{product.price}</p>
-          <button onClick={() => addToCart(product)}>Add to cart</button>
+          <h4>{product.price}</h4>
+          <p>
+            <button onClick={() => buyNow(product)}>Buy now</button>
+          </p>
         </div>
       ))}
     </div>
