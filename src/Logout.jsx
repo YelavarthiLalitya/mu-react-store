0592261b1969f89b5c6d95d8d1d705230f44d243
YelavarthiLalitya <lalityayelavarthi@gmail.com>
2025-06-10 15:38:38 +0530

    updated

--- src/Logout.jsx
+++ src/Logout.jsx
@@ -1,14 +1,11 @@
-import React from 'react'
-import { AppContext } from './App'
-import { useContext } from 'react'
-import { useNavigate } from 'react-router-dom';
-
+import React from "react";
+import App, { AppContext } from "./App";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 export default function Logout() {
-    const {email, setEmail} = useContext(AppContext);
-    const Navigate = useNavigate();
-    setEmail(null);
-    Navigate("/")
-  return (
-    <div>Logout</div>
-  )
+  const { email, setEmail } = useContext(AppContext);
+  const Navigate = useNavigate()
+  setEmail(null);
+  Navigate("/")
+  return <div>Logout</div>;
 }
